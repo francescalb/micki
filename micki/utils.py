fd498bc16ddc9bb24e74ae81bbbe9fe8e4cf4eff
Francesca Lønstad Bleken <francesca.l.bleken@sintef.no>
2021-01-15 14:24:58 +0100

    Fixed mixed indentation spaces/tab and print fir py3

--- micki/utils.py
+++ micki/utils.py
@@ -33,25 +33,25 @@ def calculate_avg_vdw_radius(atoms, npoints=8001):
             Xparr = vec * np.dot(pos[j], vec)
             Xperp = pos[j] - Xparr
             Ratom = rad[j]
-	    Rperp = np.linalg.norm(Xperp)
-	    # Skip atom if line doesn't intersect its vdW sphere
-	    if Rperp > Ratom:
-	        continue
-	    Rparr = np.linalg.norm(Xparr) * np.sign(np.dot(pos[j], vec))
-	    b = -2 * Rperp
-	    c = Rperp**2 - Ratom**2
-	    d1 = np.abs((-b + np.sqrt(b**2 - 4 * c))/2)
-	    d2 = np.abs((-b - np.sqrt(b**2 - 4 * c))/2)
-	    if d1 > Ratom and d2 < Ratom:
-	        d = d2
-	    elif d1 < Ratom and d2 > Ratom:
-	        d = d1
-	    elif d1 > Ratom and d2 > Ratom:
-	        raise ValueError, 'Error! Both distances greater than Ratom!'
-	    elif d1 < Ratom and d2 < Ratom:
-	        raise ValueError, 'Error! Both distances less than Ratom!'
-            else:
-                raise RuntimeError, "This should be impossible! d1 = {}, d2 = {}, Ratom = {}".format(d1, d2, Ratom)
-	    if Rparr + d > Rmol[i]:
-	        Rmol[i] = Rparr + d
+        Rperp = np.linalg.norm(Xperp)
+        # Skip atom if line doesn't intersect its vdW sphere
+        if Rperp > Ratom:
+            continue
+        Rparr = np.linalg.norm(Xparr) * np.sign(np.dot(pos[j], vec))
+        b = -2 * Rperp
+        c = Rperp**2 - Ratom**2
+        d1 = np.abs((-b + np.sqrt(b**2 - 4 * c))/2)
+        d2 = np.abs((-b - np.sqrt(b**2 - 4 * c))/2)
+        if d1 > Ratom and d2 < Ratom:
+            d = d2
+        elif d1 < Ratom and d2 > Ratom:
+            d = d1
+        elif d1 > Ratom and d2 > Ratom:
+            raise ValueError('Error! Both distances greater than Ratom!')
+        elif d1 < Ratom and d2 < Ratom:
+            raise ValueError('Error! Both distances less than Ratom!')
+        else:
+            raise RuntimeError("This should be impossible! d1 = {}, d2 = {}, Ratom = {}".format(d1, d2, Ratom))
+        if Rparr + d > Rmol[i]:
+            Rmol[i] = Rparr + d
     return np.average(Rmol)
